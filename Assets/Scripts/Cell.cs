585a66becaab3b07b6b25b8e9c4872fad5fb7fe2
Dinosaur <44901846+Slavon7@users.noreply.github.com>
2024-11-06 21:41:27 +0200

    Update Cell.cs

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -4,8 +4,6 @@ public class Cell : MonoBehaviour
 {
     public bool ContainsSquare()
     {
-        // Здесь вы можете реализовать логику для проверки наличия квадрата на клетке
-        // Например, можно проверить наличие дочерних объектов
         return transform.childCount > 0;
     }
 }
